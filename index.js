24e4b3f5d4a7aba5717d6fe65770ceb65b7dd008
joshnajera <switch620@gmail.com>
2024-03-11 15:03:44 -0700

    initial setup

---
+++ index.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const form = document.querySelector("#searchForm");
+
+  form.addEventListener("submit", function (e) {
+    e.preventDefault(); // Prevent the form from submitting
+
+    // Your code here
+  });
+});
